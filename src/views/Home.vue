b2ca0ab5a746a20a1941bbf16b37d0923a478363
ivoderuever <ivoderuever@gmail.com>
2021-06-05 17:23:05 +0200

    Add a homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    
+  <div class="home-center">
+    <img class="logo" alt="logo" src="../assets/Logo.svg" />
+    <h1>TCG Wiki</h1>
+    <p class="small-info">
+      Welcome to TCG Wiki. Click on a set to start looking at cards.
+    </p>
+    <div class="links">
+      <a class="link" href="https://github.com/ivoderuever/tcg-wiki" target="_blank">
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <g data-name="Layer 2">
+            <rect width="24" height="24" opacity="0" />
+            <path
+              d="M16.24 22a1 1 0 0 1-1-1v-2.6a2.15 2.15 0 0 0-.54-1.66 1 1 0 0 1 .61-1.67C17.75 14.78 20 14 20 9.77a4 4 0 0 0-.67-2.22 2.75 2.75 0 0 1-.41-2.06 3.71 3.71 0 0 0 0-1.41 7.65 7.65 0 0 0-2.09 1.09 1 1 0 0 1-.84.15 10.15 10.15 0 0 0-5.52 0 1 1 0 0 1-.84-.15 7.4 7.4 0 0 0-2.11-1.09 3.52 3.52 0 0 0 0 1.41 2.84 2.84 0 0 1-.43 2.08 4.07 4.07 0 0 0-.67 2.23c0 3.89 1.88 4.93 4.7 5.29a1 1 0 0 1 .82.66 1 1 0 0 1-.21 1 2.06 2.06 0 0 0-.55 1.56V21a1 1 0 0 1-2 0v-.57a6 6 0 0 1-5.27-2.09 3.9 3.9 0 0 0-1.16-.88 1 1 0 1 1 .5-1.94 4.93 4.93 0 0 1 2 1.36c1 1 2 1.88 3.9 1.52a3.89 3.89 0 0 1 .23-1.58c-2.06-.52-5-2-5-7a6 6 0 0 1 1-3.33.85.85 0 0 0 .13-.62 5.69 5.69 0 0 1 .33-3.21 1 1 0 0 1 .63-.57c.34-.1 1.56-.3 3.87 1.2a12.16 12.16 0 0 1 5.69 0c2.31-1.5 3.53-1.31 3.86-1.2a1 1 0 0 1 .63.57 5.71 5.71 0 0 1 .33 3.22.75.75 0 0 0 .11.57 6 6 0 0 1 1 3.34c0 5.07-2.92 6.54-5 7a4.28 4.28 0 0 1 .22 1.67V21a1 1 0 0 1-.94 1z"
+            />
+          </g>
+        </svg>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -9,9 +25,41 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     // HelloWorld
+  },
+};
+</script>
+
+<style lang="scss">
+.home-center {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+}
+
+.logo {
+  width: auto;
+  height: 80px;
+  margin-bottom: 10px;
+}
+
+.small-info {
+  width: 350px;
+  text-align: center;
+  padding: 10px 10px;
+}
+
+.link {
+  .icon {
+    width: 25px;
+    height: 25px;
+    fill: var(--font);
   }
 }
-</script>
+</style>
